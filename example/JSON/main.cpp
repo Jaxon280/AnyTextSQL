50e48f999e5d3821c1fb163f735ad54bf4ec2297
natsuoiida <natsuoiida@deneb.eidos.ic.i.u-tokyo.ac.jp>
2021-12-30 19:44:25 +0900

    add bench

--- example/JSON/main.cpp
+++ example/JSON/main.cpp
@@ -14,7 +14,20 @@ int main(int argc, char *argv[]) {
     }
 
     VFALexer lexer(data, 0, size);
+
+#if (defined BENCH)
+    timeval lex1, lex2;
+    gettimeofday(&lex1, NULL);
+#endif
+
     std::vector<Token> tokens = lexer.lex();
+
+#if (defined BENCH)
+    gettimeofday(&lex2, NULL);
+    printf(
+        "\n0 | #BENCH_Lex %lf\n\n",
+        (lex2.tv_sec - lex1.tv_sec) + (lex2.tv_usec - lex1.tv_usec) * 0.000001);
+#endif
 
     int cnt = 0;
     for (Token &token : tokens) {
