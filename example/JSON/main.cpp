16ee8b17865fe1ff4a35a7e3a161dbfbe13d00fd
natsuoiida <natsuoiida@spica.eidos.ic.i.u-tokyo.ac.jp>
2022-01-31 18:15:44 +0900

    fix benchmark

--- example/JSON/main.cpp
+++ example/JSON/main.cpp
@@ -6,6 +6,16 @@
 #include "vfa.hpp"
 
 int main(int argc, char *argv[]) {
+#if (defined BENCH)
+    timeval total1, total2;
+    gettimeofday(&total1, NULL);
+#endif
+
+#if (defined BENCH)
+    timeval disk1, disk2;
+    gettimeofday(&disk1, NULL);
+#endif
+
     int fd = open(argv[1], O_RDONLY);
     struct stat fst;
     fstat(fd, &fst);
@@ -18,25 +28,53 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    VFALexer lexer(data, 0, size);
-
 #if (defined BENCH)
-    timeval lex1, lex2;
-    gettimeofday(&lex1, NULL);
+    gettimeofday(&disk2, NULL);
+    printf("\n0 | #BENCH_Disk %lf\n\n",
+           (disk2.tv_sec - disk1.tv_sec) +
+               (disk2.tv_usec - disk1.tv_usec) * 0.000001);
 #endif
+    const int epochs = 30;
 
-    std::vector<Token> tokens = lexer.lex();
-
-#if (defined BENCH)
-    gettimeofday(&lex2, NULL);
-    printf(
-        "\n0 | #BENCH_Lex %lf\n\n",
-        (lex2.tv_sec - lex1.tv_sec) + (lex2.tv_usec - lex1.tv_usec) * 0.000001);
-#endif
-
+    double ex_time = 0.0;
     int cnt = 0;
-    for (Token &token : tokens) {
-        cnt++;
+
+    for (int i = 0; i < epochs; i++) {
+        VFALexer lexer(data, 0, size);
+        std::vector<Token> tokens;
+#if (defined BENCH)
+        timeval lex1, lex2;
+        gettimeofday(&lex1, NULL);
+#endif
+        tokens = lexer.lex();
+        // int count = lexer.lex();
+
+#if (defined BENCH)
+        gettimeofday(&lex2, NULL);
+        ex_time += (lex2.tv_sec - lex1.tv_sec) +
+                   (lex2.tv_usec - lex1.tv_usec) * 0.000001;
+#endif
+        for (Token &token : tokens) {
+            cnt++;
+        }
     }
-    printf("%d\n", cnt);
+
+#if (defined BENCH)
+    printf("#BENCH_Lex %lf s\n\n", ex_time);
+    printf("#File size: %d MB\n\n", (size >> 20) * epochs);
+    printf("#Throughput: %lf MB/s\n\n",
+           ((double)(size >> 20) * (double)epochs / ex_time));
+#endif
+
+    // printf("restaurant count: %d\n", count);
+    // printf("above 4.5 restaurants: %d\n", count);
+
+    printf("stars count: %d\n", cnt);
+
+#if (defined BENCH)
+    gettimeofday(&total2, NULL);
+    printf("\n0 | #BENCH_Total %lf\n\n",
+           (total2.tv_sec - total1.tv_sec) +
+               (total2.tv_usec - total1.tv_usec) * 0.000001);
+#endif
 }
