801ea6295c67be66efdb1aa16da9bb3bdc500c59
Natsuo Iida <keresu0720@g.ecc.u-tokyo.ac.jp>
2022-07-12 09:33:32 +0900

    add DFA construction (powerset)

--- include/dfa.hpp
+++ include/dfa.hpp
@@ -1,109 +1,91 @@
 #pragma once
 
+#include "c/regex_tree.h"
 #include "common.hpp"
-#include "nfa.hpp"
 
 namespace vlex {
 class DFA {
    public:
     struct SubMatchStates {
-        ST_TYPE startState;
+        DFA_ST_TYPE startState;
         bool isAnyStart;
-        ST_TYPE endState;
+        DFA_ST_TYPE endState;
         bool isAnyEnd;
 
-        SubMatchStates(ST_TYPE _startState, bool _isAnyStart, ST_TYPE _endState,
-                       bool _isAnyEnd)
+        SubMatchStates(DFA_ST_TYPE _startState, bool _isAnyStart,
+                       DFA_ST_TYPE _endState, bool _isAnyEnd)
             : startState(_startState),
               isAnyStart(_isAnyStart),
               endState(_endState),
               isAnyEnd(_isAnyEnd) {}
+        SubMatchStates() {}
     };
+    using TransTable = std::vector<std::vector<DFA_ST_TYPE>>;
+    using SubMatches = std::vector<SubMatchStates>;
+    using StateSet = std::set<DFA_ST_TYPE>;
 
    private:
-    std::vector<std::vector<ST_TYPE>> transTable;
-    std::set<ST_TYPE> acceptStates;
+    TransTable transTable;
+    StateSet acceptStates;
+    SubMatches subMatches;
     int numStates;
-
-    // inherit from NFA
-    std::vector<State> states;
-    State initState;
-    State acceptState;
-    std::vector<SubMatchStates> subMatches;
-    std::vector<Transition> transVec;
+    int initState;
 
    public:
-    DFA(std::vector<std::vector<ST_TYPE>> _transTable,
-        std::vector<State> _states, State _acceptState,
-        std::set<ST_TYPE> _acceptStates, int _numStates)
-        : transTable(_transTable),
-          states(_states),
-          acceptStates(_acceptStates),
-          initState(INIT_STATE),
-          numStates(_numStates) {}
-
-    DFA(std::vector<std::vector<ST_TYPE>> _transTable,
-        std::set<ST_TYPE> _acceptStates,
-        std::vector<SubMatchStates> _subMatches, int _numStates)
+    DFA() {}
+    DFA(int _initState, std::vector<int>& _powsStates,
+        std::map<int, int>& _old2new,
+        std::map<int, std::vector<int>>& _powTTable,
+        std::set<int>& _acceptStates, std::vector<SubMatchStates> _smses);
+    DFA(TransTable _transTable, StateSet _acceptStates, SubMatches _subMatches,
+        int _numStates)
         : transTable(_transTable),
           acceptStates(_acceptStates),
           subMatches(_subMatches),
           numStates(_numStates) {}  // used in test
+    ~DFA() {}
 
-    inline std::vector<std::vector<ST_TYPE>> getTransTable() {
-        return transTable;
-    }
-    inline std::set<ST_TYPE> getAcceptStates() { return acceptStates; }
+    inline TransTable& getTransTable() { return transTable; }
+    inline SubMatches& getSubMatches() { return subMatches; }
+    inline StateSet& getAcceptStates() { return acceptStates; }
     inline int getNumStates() { return numStates; }
-    inline std::vector<SubMatchStates>& getSubMatchStates() {
-        return subMatches;
-    }
-    void set_nfa(std::vector<State> _states, State _initState,
-                 State _acceptState, std::vector<Transition> _transVec) {
-        states = _states;
-        initState = _initState;
-        acceptState = _acceptState;
-        transVec = _transVec;
-    }
 };
 
 class DFAGenerator {
    private:
-    std::map<ST_TYPE, std::vector<ST_TYPE>> epsilonTable;
-    // std::map<ST_TYPE, std::vector<Transition>> NFATransMap;
-    std::map<uint8_t, std::vector<Transition>> NFATransMap;
-    std::stack<std::vector<ST_TYPE>> stateStack;
+    std::vector<std::vector<int>> epsilonTable;  // NFA state -> NFA states
+    std::vector<std::vector<std::set<int>>>
+        powsetTransTable;  // NFA state -> NFA powset state table (128)
+
+    std::stack<int> stateStack;
+
+    NFA* nfa;
+
+    std::map<int, std::set<int>>
+        powsetStates;  // DFA state -> NFA powset states
+    std::map<int, std::vector<int>> transTable;  // state -> NFA powset state
+    std::vector<DFA::SubMatchStates> smses;
+    std::map<int, int> default2mini;
+    std::set<int> acceptStates;
+    DFA* dfa;
+
+    std::vector<int> stateVec;  // minimized DFA states
+
+    void setEpsilonTable(Transition* trans, int transSize, int stateSize);
+    void setPowsetTable(Transition* trans, int transSize);
+    void initPowsetStates(int initState);
+    void minimize();
 
    public:
-    // DFAGenerator() {}
-    // DFA construct_dfa(NFA& nfa) {
-    //     for (Transition& t : nfa.get_transVec()) {
-    //         if (t.get_c() == EPSILON) {
-    //             epsilonTable[t.get_start().get_id()].push_back(
-    //                 t.get_end().get_id());
-    //         } else {
-    //             // NFATransMap[t.get_start().get_id()].push_back(t);
-    //             NFATransMap[(uint8_t)t.get_c()].push_back(t);
-    //         }
-    //     }
+    DFAGenerator(NFA* _nfa) : nfa(_nfa) {
+        int stateSize = _nfa->stateSize;
 
-    //     std::set<ST_TYPE>
-    //     initState(epsilonTable[nfa.get_initState().get_id()]);
-    //     stateStack.push(initState);
-    //     while (!stateStack.empty()) {
-    //         std::set<ST_TYPE> state = stateStack.pop();
-    //         for (uint8_t c = 0; c < 127; c++) {
-    //             std::set<ST_TYPE> new_state;
-    //             for (Transition& t : NFATransMap[c]) {
-    //                 if (state.find(t.get_start().get_id()) != state.end()) {
-    //                     new_state.insert(t.get_end().get_id());
-    //                 }
-    //             }
-
-    //             if (new_state.size() > 0) {
-    //             }
-    //         }
-    //     }
-    // }
+        epsilonTable = std::vector<std::vector<int>>(stateSize);
+        powsetTransTable = std::vector<std::vector<std::set<int>>>(
+            stateSize, std::vector<std::set<int>>(ASCII_SZ));
+    }
+    ~DFAGenerator() { delete dfa; }
+    void generate();
+    DFA* getDFA() { return dfa; }
 };
 }  // namespace vlex
---
+++ src/dfa.cpp
@@ -0,0 +1,246 @@
+#include "dfa.hpp"
+
+using namespace vlex;
+
+DFA::DFA(int _initState, std::vector<int>& _powsStates,
+         std::map<int, int>& _old2new,
+         std::map<int, std::vector<int>>& _powTTable,
+         std::set<int>& _acceptStates, std::vector<SubMatchStates> _smses) {
+    std::map<int, int> old2new_comp;
+    numStates = 0;
+    for (const int& s : _powsStates) {
+        old2new_comp[s] = numStates;
+        numStates++;
+    }
+
+    for (const auto& [os, ns] : _old2new) {
+        old2new_comp[os] = old2new_comp[ns];
+    }
+
+    initState = _initState;
+    for (const int& ps : _powsStates) {
+        if (_acceptStates.count(ps) > 0) {
+            acceptStates.insert(old2new_comp[ps]);
+        }
+    }
+
+    transTable = std::vector(numStates, std::vector<DFA_ST_TYPE>(ASCII_SZ));
+    int j = 0;
+    for (const int& ps : _powsStates) {
+        for (int i = 0; i < ASCII_SZ; i++) {
+            if (ps == INV_STATE) {
+                transTable[j][i] = INV_STATE;
+            } else {
+                transTable[j][i] = old2new_comp[_powTTable[ps][i]];
+            }
+        }
+        j++;
+    }
+
+    for (SubMatchStates& sms : _smses) {
+        SubMatchStates new_sms = sms;
+        new_sms.startState = old2new_comp[sms.startState];
+        new_sms.endState = old2new_comp[sms.endState];
+        subMatches.push_back(new_sms);
+    }
+}
+
+void DFAGenerator::setEpsilonTable(Transition* trans, int transSize,
+                                   int stateSize) {
+    std::vector<std::vector<int>> etTable(stateSize);
+    for (int ti = 0; ti < transSize; ti++) {
+        if (trans[ti].c == EPSILON) {
+            etTable[trans[ti].start].push_back(trans[ti].end);
+        }
+    }
+
+    for (int s = 0; s < stateSize; s++) {
+        std::stack<int> sstack;
+        std::vector<int> visited(stateSize, 0);
+
+        sstack.push(s);
+        while (!sstack.empty()) {
+            int v = sstack.top();
+            sstack.pop();
+
+            if (visited[v] == 0) {
+                visited[v] = 1;
+                for (int ve : etTable[v]) {
+                    if (visited[ve] == 0) {
+                        sstack.push(ve);
+                        epsilonTable[s].push_back(ve);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void DFAGenerator::setPowsetTable(Transition* trans, int transSize) {
+    for (int ti = 0; ti < transSize; ti++) {
+        int s = trans[ti].start;
+        int e = trans[ti].end;
+        int c = trans[ti].c;
+
+        if (c == EPSILON) continue;
+        powsetTransTable[s][c].insert(e);
+
+        if (epsilonTable[e].size() > 0) {
+            for (int es : epsilonTable[e]) {
+                if (powsetTransTable[s][c].count(es) == 0) {
+                    powsetTransTable[s][c].insert(es);
+                }
+            }
+        }
+    }
+}
+
+void DFAGenerator::initPowsetStates(int initState) {
+    std::set<int> pset;
+    powsetStates.insert(std::pair<int, std::set<int>>(initState, pset));
+    powsetStates[initState].insert(nfa->initState);
+    for (int es : epsilonTable[nfa->initState]) {
+        powsetStates[initState].insert(es);
+    }
+    if (powsetStates[initState].count(nfa->acceptState) > 0) {
+        acceptStates.insert(initState);
+    }
+}
+
+void DFAGenerator::minimize() {
+    std::stack<int> sstack;
+    sstack.push(INIT_STATE);
+
+    std::vector<int> visited(powsetStates.size() + 1, 0);
+    stateVec.push_back(0);  // inv state
+    while (!sstack.empty()) {
+        int s = sstack.top();
+        sstack.pop();
+
+        if (visited[s] == 1) {
+            continue;
+        } else {
+            visited[s] = 1;
+            default2mini[s] = s;
+            stateVec.push_back(s);
+        }
+
+        for (const auto& [sid, ps] : powsetStates) {
+            if (sid == s) continue;
+            if ((acceptStates.count(s) > 0) ^ (acceptStates.count(sid) > 0)) {
+                sstack.push(sid);
+            }
+
+            for (int i = 0; i < ASCII_SZ; i++) {
+                if (transTable[s][i] != transTable[sid][i]) {
+                    sstack.push(sid);
+                    break;
+                }
+
+                if (i == ASCII_SZ - 1) {
+                    // merge
+                    visited[sid] = 1;
+                    for (int pss : ps) {
+                        powsetStates[s].insert(pss);
+                    }
+                    default2mini[sid] = s;  // memo
+                }
+            }
+        }
+    }
+}
+
+DFAGenerator::~DFAGenerator() { delete dfa; }
+
+void DFAGenerator::generate() {
+    setEpsilonTable(nfa->transVec, nfa->transSize, nfa->stateSize);
+    setPowsetTable(nfa->transVec, nfa->transSize);
+
+    int initState = INIT_STATE;
+    initPowsetStates(initState);
+
+    stateStack.push(initState);
+    int stateID = 2;
+    while (!stateStack.empty()) {
+        int s = stateStack.top();
+        stateStack.pop();
+
+        if (transTable.count(s) == 0) {
+            transTable[s] = std::vector<int>(ASCII_SZ, INV_STATE);
+        }
+
+        int sc = 0;
+        int new_s;
+
+        while (sc < ASCII_SZ) {
+            std::set<int> sset;
+            for (int ss : powsetStates[s]) {
+                for (int se : powsetTransTable[ss][sc]) {
+                    sset.insert(se);
+                }
+            }
+
+            if (sset.size() == 0) {
+                transTable[s][sc] = INV_STATE;
+                goto loop_end;
+            }
+
+            for (const auto& [sid, ps] : powsetStates) {
+                if (sset == ps) {
+                    transTable[s][sc] = sid;
+                    goto loop_end;
+                }
+            }
+            new_s = stateID;
+            stateID++;
+            powsetStates.insert(std::pair<int, std::set<int>>(new_s, sset));
+            if (sset.count(nfa->acceptState) > 0) {
+                acceptStates.insert(new_s);
+            }
+            transTable[s][sc] = new_s;
+            stateStack.push(new_s);
+
+        loop_end:
+            sc++;
+        }
+    }
+
+    minimize();
+
+    for (SubMatch* s = nfa->subms; s != NULL; s = s->next) {
+        DFA::SubMatchStates sms;
+        for (int ss : stateVec) {
+            if (powsetStates[ss].count(s->start) > 0) {
+                sms.startState = ss;
+                sms.isAnyStart = s->isAnyStart;
+                // todo: type and subm name
+            }
+
+            if (powsetStates[ss].count(s->end) > 0) {
+                sms.endState = ss;
+                sms.isAnyEnd = s->isAnyEnd;
+            }
+        }
+        smses.push_back(sms);
+    }
+
+    dfa = new DFA(initState, stateVec, default2mini, transTable, acceptStates,
+                  smses);
+    // std::cout << "Num States: " << dfa->getNumStates() << std::endl;
+    // std::vector<std::vector<DFA_ST_TYPE>>& ttable = dfa->getTransTable();
+    // for (int ti = 1; ti < ttable.size(); ti++) {
+    //     for (int tc = 0; tc < ASCII_SZ; tc++) {
+    //         if (ttable[ti][tc] != 0) {
+    //             std::cout << "cur state: " << ti << " -- " << (char)tc
+    //                       << " --> " << (int)ttable[ti][tc] << std::endl;
+    //         }
+    //     }
+    // }
+    // for (DFA_ST_TYPE s : dfa->getAcceptStates()) {
+    //     std::cout << "Accept States: " << (int)s << std::endl;
+    // }
+    // for (DFA::SubMatchStates& s : dfa->getSubMatches()) {
+    //     std::cout << (int)s.startState << (int)s.isAnyStart << " "
+    //               << (int)s.endState << (int)s.isAnyEnd << std::endl;
+    // }
+}
